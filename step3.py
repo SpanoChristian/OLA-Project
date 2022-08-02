c98e559b974917d296922f36c52a520c3ec3404f
Raffaello Fornasiere <fornasiere.raffaello@gmail.com>
2022-08-02 17:53:40 +0200

    update

--- step3.py
+++ step3.py
@@ -6,70 +6,82 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
+# alpha function to be learnt
 def fun(x, x_bar, speed):
     return x_bar * (1.0 - np.exp(-x * speed))
 
 
 logging.debug("setting up parameters")
-sub_campaigns = 5
-alpha_bar = np.array(range(1, sub_campaigns + 1))
-speeds = np.random.normal([1, 1, 1, 1, 1], 0.2)
-sigmas = np.array([3 for i in range(sub_campaigns)])
-max_budget = max(speeds)  # half the maximum required to reach the saturation of all the arms
-n_arms = 6
-arms = np.linspace(0.0, speeds, n_arms)
+
+
+class Config(object):
+    pass
+
+
+config = Config()
+config.sub_campaigns = 5
+config.alpha_bar = np.array(range(1, config.sub_campaigns + 1))
+config.speeds = np.random.normal([1, 1, 1, 1, 1], 0.2)
+config.sigmas = np.array([3 for i in range(config.sub_campaigns)])
+config.max_budget = sum(config.speeds) * 5 / 2  # half the maximum required to reach the saturation of all the arms
+config.n_arms = 6
+config.arms = np.linspace(0.0, config.speeds, config.n_arms).T
 
 logging.debug(f'''
-number of subcampaigns:     {speeds},
-alpha_bars:                 {alpha_bar},
-speeds:                     {speeds},
-sigmas:                     {sigmas},
-max_budget:                 {max_budget},
-n_arms:                     {n_arms},
-arms:                       {arms}
+number of subcampaigns:     {config.speeds},
+alpha_bars:                 {config.alpha_bar},
+speeds:                     {config.speeds},
+sigmas:                     {config.sigmas},
+max_budget:                 {config.max_budget},
+n_arms:                     {config.n_arms},
+arms:                       {config.arms}
 ''')
 
 env = Environment()
 
-for i in range(sub_campaigns):
-    sc = Subcampaign(budgets=arms[i], function=lambda x: fun(x, alpha_bar[i], speeds[i]), sigma=sigmas[i])
+for i in range(config.sub_campaigns):
+    sc = Subcampaign(budgets=config.arms[i], function=lambda x: fun(x, config.alpha_bar[i], config.speeds[i]),
+                     sigma=config.sigmas[i])
     env.add_subcampaign(subcampaign=sc)
 
 print("created subcampaigns")
 
 learners = []
-for i in range(0, len(alpha_bar)):
-    learners.append(GPTS_Learner(arms=arms, n_arms=n_arms))
+for i in range(0, len(config.alpha_bar)):
+    learners.append(GPTS_Learner(arms=config.arms, n_arms=config.n_arms))
 
 
-def greedy_knapsack(table, budget):
-    end = False
-    res = np.zeros(table.shape[0])
-    while not end:
-        i_max = np.unravel_index(np.argmax(table), table.shape)
-        if table[i_max] < budget:
-            res[i_max[0]] = i_max[1]
-            budget -= table[i_max]
-            table = np.delete(table, i_max[0], 0)
-        else:
-            table[i_max] = 0
-        if table.shape[0] == 0 or np.min(table) > budget:
-            end = True
+def greedy_knapsack(original_table, budget):
+    if sum([np.min(x) for x in original_table]) > budget:
+        raise Exception("Impossible optimization problem")
 
-    return res
+    table = original_table.copy()
+    res = [np.argmin(x) for x in table]
+    budget -= sum([x[res[i]] for i, x in enumerate(table)])
+    while budget >= 0 and np.max(table) >= 0:
+        best = np.unravel_index(np.argmin(abs(table - budget)), table.shape)
+        complementary = table[best] - table[best[0]][res[best[0]]]
+        if complementary <= budget and best[1] != res[best[0]]:
+            budget -= complementary
+            res[best[0]] = best[1]
+
+        table[best[0]] = np.ones(table.shape[1]) * float('-inf')
+
+    return res, sum([x[res[i]] for i, x in enumerate(original_table)])
 
 
-m = np.array([[1, 2, 3], [4, 6, 7], [7, 8, 9]])
-greedy_knapsack(m, 14)
+# m = np.array([[1, 2, 3], [4, 6, 7], [7, 8, 9]])
+# res, total = greedy_knapsack(m, 14)
 
 T = 60
 for i in range(0, T):
-    samples = np.array([])
+    samples = np.zeros(shape=(0, config.n_arms))
     for learner in learners:
-        np.append(samples, (np.array(learner.pull_all_arms())))
+        tmp = np.array(learner.pull_all_arms())
+        samples = np.append(samples, [tmp], axis=0)
 
-    arms = greedy_knapsack(m, max_budget)
+    arms, total = greedy_knapsack(samples.copy(), config.max_budget)
 
-    for j in range(sub_campaigns):
-        arm_reward = env.round(subcampaign=j, pulled_arm=arms[i])
+    for j in range(config.sub_campaigns):
+        arm_reward = env.round(subcampaign=j, pulled_arm=config.arms[i])
         learners[j].update_observations(j, arm_reward)
