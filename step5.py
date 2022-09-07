d0f6c77d35187f3fdc3db73690288c7c17702bfa
Raffaello Fornasiere <fornasiere.raffaello@gmail.com>
2022-09-07 13:11:27 +0200

    fix vari e refactor

--- step5.py
+++ step5.py
@@ -1,24 +1,20 @@
+from Environments.Base_Environment import *
 from Environments.Environment_step5 import *
 from Learners.GPTS_Learner import *
-from Learners.GPUCB_Learner import *
 import logging
+from utils.knapsack import *
 import warnings
 import matplotlib.gridspec as gridspec
 from utils.Optimization_Algorithm import *
 import matplotlib.pyplot as plt
-
-plt.ion()
+import time
+from utils.MKCP import *
+from Runner import *
 
 logging.basicConfig(level=logging.DEBUG)
 warnings.filterwarnings("ignore")
-
-
-# alpha function to be learnt
-def fun(x, x_bar, speed):
-    return x_bar * (1.0 - np.exp(-x * speed))
-
-
-logging.debug("setting up parameters")
+logging.getLogger('matplotlib.pyplot').disabled = True
+logging.getLogger('matplotlib.font_manager').disabled = True
 
 
 class Config(object):
@@ -26,137 +22,62 @@ class Config(object):
 
 
 config = Config()
-config.sub_campaigns = 5
-config.alpha_bar = np.array([i + 10 for i in range(1, config.sub_campaigns + 1)])
-config.speeds = np.random.normal([0.5, 0.3, 0.4, 0.5, 0.6], 0.0)
-config.sigmas = np.array([0.05 * (i + 1) for i in range(config.sub_campaigns)])
-config.max_budget = sum(config.speeds) * 5
-config.n_arms = 10
-config.arms = np.linspace(0.0, config.max_budget, config.n_arms).T
+config.n_subcampaigns = 5
+config.ratios = to_sum_1(np.array([3, 2, 3, 4, 5, 6]))
+config.alpha_bars = config.ratios[1:]
+config.speeds = [np.random.uniform(0.1, 0.9) for i in range(config.n_subcampaigns)]
+config.opponent = config.ratios[0]
+config.adj_matrix = np.array([
+    [0, 0, 0.04, 0.07, 0.9],
+    [0, 0, 0.03, 0.03, 0.7],
+    [0, 0, 0, 0.2, 0.5],
+    [0.02, 0.02, 0, 0, 0],
+    [0, 0.01, 0, 0, 0]
+])
+config.budgets = np.linspace(0, sum(5 / np.array(config.speeds)) / 2, 300)
 
-config.adj_matrix = np.array([[0, 0, 0.2, 0, 0],
-                              [0.1, 0, 0, 0.3, 0],
-                              [0, 0.2, 0, 0.1, 0],
-                              [0.2, 0, 0, 0, 0],
-                              [0, 0, 0, 0, 0]
-                              ])
-# p2 -> p1, p4
-# p1 -> p3
-#
-config.lambda_param = 0.5
-config.second_secondary = np.array([[0, 0, 0, 0, 0],
-                                    [0, 0, 0, 1, 0],
-                                    [1, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0]])
+env = Environment5(n_subcampaigns=config.n_subcampaigns,
+                   subcampaign_class=Subcampaign5,
+                   alpha_bars=config.alpha_bars,
+                   multiplier=10000,
+                   speeds=config.speeds,
+                   opponent=config.opponent,
+                   adj_matrix=config.adj_matrix,
+                   sigma_matrix=0.001,
+                   budgets=config.budgets,
+                   daily_clicks=100
+                   )
 
-m = np.array(0)
-for i in range(config.sub_campaigns):
-    for j in range(config.n_arms):
-        print(fun(config.arms[j], config.alpha_bar[i], config.speeds[i]), ',', end='')
-    print('\n')
+runner = Runner(environment=env, optimizer=mkcp_solver, lernerClass=GPTS_Learner, dont_update_before=1)
 
-logging.debug(f'''
-number of subcampaigns:     {config.speeds},
-alpha_bars:                 {config.alpha_bar},
-speeds:                     {config.speeds},
-sigmas:                     {config.sigmas},
-max_budget:                 {config.max_budget},
-n_arms:                     {config.n_arms},
-arms:                       {config.arms}
-''')
+start = time.time()
+T = 40
+runner.run(T)
 
-env = Environment5(config.adj_matrix, 0.02, 1000)
+gs = gridspec.GridSpec(1, 2)
+plt.figure(figsize=(13, 5))
 
-
-
-
-for i in range(config.sub_campaigns):
-    sc = Subcampaign(budgets=config.arms, function=lambda x: fun(x, config.alpha_bar[i], config.speeds[i]))
-    env.add_subcampaign(subcampaign=sc)
-
-print("created subcampaigns")
-
-learners = []
-for i in range(0, len(config.alpha_bar)):
-    learners.append(GPTS_Learner(arms=config.arms, n_arms=config.n_arms))
-
-aux = []
-for i in range(0, 100):
-    k = np.array(env.round())
-    sol = optimization_algorithm(np.array(env.round()))
-    aux.append(sol)
-
-list = [{'elem': item, 'count': aux.count(item)} for item in aux]
-max = max(range(len(list)), key=lambda i: list[i]['count'])
-clairvoyant_solution = list[max]['elem']
-k = [np.random.normal(10, 1)]
-env.next_day(clairvoyant_solution)
-k.extend([env.get_reward(subcampaign) for subcampaign in range(config.sub_campaigns)])
-clairvoyant_reward = sum(k[1:])
-
-T = 101
-x = [[] for i in range(5)]
-y = [[] for i in range(5)]
-y_pred = [[] for i in range(5)]
-x_pred = config.arms
-sigmas = [[] for i in range(5)]
-gs = gridspec.GridSpec(1, 5)
-
-for i in range(0, T):
-    samples = np.zeros(shape=(0, config.n_arms))
-    for learner in learners:
-        tmp = np.array(learner.pull_all_arms())
-        samples = np.append(samples, [tmp], axis=0)
-
-    arms = optimization_algorithm(samples)
-
-    env.next_day(arms)
-
-    for j in range(config.sub_campaigns):
-        arm_reward = env.get_reward(j)
-
-        x[j].append(arms[j])
-        y[j].append(arm_reward)
-
-        if i < 50 and i % 10 != 0:
-            learners[j].update_observations(arms[j], arm_reward)
-        else:
-            learners[j].update(arms[j], arm_reward)
+plt.subplot(gs[0, 0])
+best_arms = mkcp_solver(np.array(env.round()))
+env.compute_rewards(best_arms)
+y_clairvoyant = [sum([env.get_reward(j) for j in range(env.n_subcampaigns)]) for i in range(T)]
 
 x = range(T)
-y = [sum([learner.collected_rewards[i] for learner in learners]) for i in range(T)]
+y = [sum([learner.collected_rewards[i] for learner in runner.learners]) for i in range(T)]
 
 plt.plot(x, y, label=u'GPTS reward')
-plt.plot(x, [clairvoyant_reward for i in range(T)], label=u'Clairvoyant reward')
+plt.plot(x, y_clairvoyant, label=u'Clairvoyant reward')
+
+plt.text(x=T * 0.75, y=np.max(y_clairvoyant) * 0.9, s=f'total time: {"{:.2f}".format(time.time() - start)}')
+
+plt.subplot(gs[0, 1])
+regret = []
+for i in range(len(x)):
+    aux = y_clairvoyant[i] - y[i] + (regret[-1] if i > 0 else 0)
+    regret.append(aux)
+
+plt.plot(x, regret)
+plt.text(x=T * 0.6, y=regret[-1] * 0.5, s=f'total regret: {"{:.2f}".format(regret[-1])}\n'
+                                          f'total clicks: {"{:.2f}".format(sum(y))}')
+
 plt.show()
-
-# if i % 10 == 0:
-#     plt.figure(figsize=(20, 5))
-#     for j in range(config.sub_campaigns):
-#         y_pred[j] = learners[j].means
-#         sigmas[j] = learners[j].sigmas
-#
-#         plt.subplot(gs[0, j])
-#
-#
-#         def n(k):
-#             return fun(k, x_bar=config.alpha_bar[j], speed=config.speeds[j])
-#
-#
-#         plt.scatter(x[j], y[j], s=5, label=u'Observed Clicks')
-#         plt.plot(x_pred, n(np.array(x_pred)), 'r', label=u'Alpha')
-#         plt.plot(x_pred, y_pred[j], 'b-', label=u'Predicted Clicks')
-#         plt.fill(np.concatenate([x_pred, x_pred[::-1]]),
-#                  np.concatenate([y_pred[j] - 1.96 * sigmas[j], (y_pred[j] + 1.96 * sigmas[j])[::-1]]),
-#                  alpha=.5, fc='b', ec='None', label='95% conf interval')
-#         plt.xlabel('$x$')
-#         plt.ylabel('$n(x)$')
-#         plt.legend(loc="lower right")
-#     plt.draw()
-#     plt.pause(0.02)
-
-plt.ioff()
-plt.show()
-
-print('ciao')
